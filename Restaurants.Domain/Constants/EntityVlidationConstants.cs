66ef75a01c7314cbc0e53733aee755ed7eaf70e7
vikto <v.atanassovv@gmail.com>
2024-04-12 21:30:12 +0300

    Add entity validation constants

---
+++ Restaurants.Domain/Constants/EntityVlidationConstants.cs
@@ -0,0 +1,21 @@
+﻿namespace Restaurants.Domain.Constants
+{
+    public static class EntityVlidationConstants
+    {
+        public static class Restaurant
+        {
+            public const int NameMaxLength = 120;
+
+            public const int DescriptionMaxLength = 800;
+
+            public const int CategoryMaxLength = 60;
+        }
+
+        public static class Dish
+        {
+            public const int NameMaxLength = 100;
+
+            public const int DescriptionMaxLength = 550;
+        }
+    }
+}

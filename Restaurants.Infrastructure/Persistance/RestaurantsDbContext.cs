ed7c17bf78f63ebcd033fb734be81b19071e9151
vikto <v.atanassovv@gmail.com>
2024-04-12 21:49:18 +0300

    dbContexts and dbSets Added

---
+++ Restaurants.Infrastructure/Persistance/RestaurantsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistance
+{
+    internal class RestaurantsDbContext : DbContext
+    {
+        internal DbSet<Restaurant> Restaurants {get;set;}
+
+        internal DbSet<Dish> Dishes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=RestaurantsDb;Trusted_Connection=True;TrustServerCertificate = True");
+        }
+    }
+}

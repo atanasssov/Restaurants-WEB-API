c59bf7a3160f15f95a4333cfd206a9aa9b28379e
vikto <v.atanassovv@gmail.com>
2024-04-13 00:19:12 +0300

    add RestaurantsController with GetAll implementation

---
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Restaurants;
+
+namespace Restaurants.API.Controllers
+{
+    [ApiController]
+    [Route("api/restaurants")]
+    public class RestaurantsController(IRestaurantsService restaurantsService) : ControllerBase
+    {
+        // Get all restaurants
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var restaurants = await restaurantsService.GetAllRestaurants();
+            return Ok(restaurants);
+        }
+    }
+}

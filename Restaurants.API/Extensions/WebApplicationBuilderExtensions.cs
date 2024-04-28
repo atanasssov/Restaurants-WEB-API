3a48e46b16a91bb07a8d210bfd28f39300070e5a
vikto <v.atanassovv@gmail.com>
2024-04-28 13:19:39 +0300

    extracting presentation layer

---
+++ Restaurants.API/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.OpenApi.Models;
+using Restaurants.API.Middlewares;
+using Serilog;
+using Serilog.Events;
+
+namespace Restaurants.API.Extensions
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static void AddPresentation(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                     {
+                          new OpenApiSecurityScheme
+                          {
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearerAuth"},
+
+                           },
+                          []
+                      }
+                });
+            });
+
+            builder.Services.AddScoped<ErrorHandlingMiddleware>();
+            builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
+
+            builder.Services.AddEndpointsApiExplorer();
+
+             // Add SeriLog to log to 
+             builder.Host.UseSerilog((context, configuration) =>
+            {
+                configuration.
+                MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
+                .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}");
+            });
+        }
+    }
+}
--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.OpenApi.Models;
-
+using Restaurants.API.Extensions;
 using Restaurants.API.Middlewares;
 using Restaurants.Application.Extensions;
 using Restaurants.Domain.Entities;
@@ -13,47 +13,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
-
+builder.AddPresentation();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearerAuth"},
-                
-            },
-            []
-        }
-    }) ;
-});
-
-
-// Add SeriLog to log to 
-builder.Host.UseSerilog((context, configuration) =>
-{
-    configuration.
-    MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
-    .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}");
-});
 
 
 var app = builder.Build();
